ccb95586223e84e874ac7a62c6c0a511af17ed46
John Huddleston <huddlej@gmail.com>
2017-09-26 16:36:16 -0700

    Added setup file for tox to install augur.

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+Pipeline components for real-time virus analysis
+Author: Richard Neher and Trevor Bedford
+"""
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "augur",
+    version = "1.0.0",
+    author = "Richard Neher and Trevor Bedford",
+    author_email = "trevor@bedford.io",
+    description = ("Pipeline components for real-time virus analysis"),
+    license = "GNU Affero General Public License v3.0",
+    keywords = "",
+    url = "https://github.com/nextstrain/augur",
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Science",
+        "License :: GNU Affero General Public License v3.0",
+    ]
+)
