ee74a8afda8a46b34b352ba64b836f22a627c440
neavemj <matthewjneave1@gmail.com>
2018-06-28 12:33:21 +1000

    initial script to download Mahar RHDV genbank records

--- builds/rhdv/get_rhdv_fastas_NCBI.py
+++ builds/rhdv/get_rhdv_fastas_NCBI.py
@@ -10,25 +10,32 @@ from Bio import SeqIO
 mahar_ids = ["MF" + str(rec) for rec in range(421563, 421702)]
 
 # use biopython go online and retrieve these records
-# need to enter an email so users don't overuse their servers
+# need to enter an email so users don't overload their servers
 
 Entrez.email = "matthewjneave1@gmail.com"
 
 def retrieve_NCBI_record(NCBI_ID):
     new_handle = Entrez.efetch(db="nucleotide", id=NCBI_ID, rettype="gb", retmode="genbank")
     seq_record = SeqIO.read(new_handle, "genbank")
+    # extracting several bits of information from different parts of the record
     collection_date = seq_record.features[0].qualifiers["collection_date"][0]
     isolate = seq_record.features[0].qualifiers["isolate"][0]
     state = isolate.split("/")[1]
     country = seq_record.features[0].qualifiers["country"][0]
     genotype = seq_record.features[0].qualifiers["note"][0].lstrip("genotype: ").replace(" ", "_")
-    print(seq_record.annotations['references'][0])
     authors = seq_record.annotations['references'][0].authors.split(",")[0] + " et al"
     title = seq_record.annotations['references'][0].title
     journal = seq_record.annotations['references'][0].journal
-    citation = title +
-    header = ">" + "|".join([isolate, "RHDV", seq_record.id, genotype, collection_date, country, state, authors])
-    print(header)
+    citation = " ".join([title, journal])
+    header = ">" + "|".join([isolate, "RHDV", seq_record.id, genotype, collection_date, country, state, authors, citation])
+    return(header, seq_record)
+
+# use this function to retrieve all the records and write to fasta file
+
+mahar_fasta = open("mahar_RHDV.fasta", "w")
+
+for record_id in mahar_ids:
+    record = retrieve_NCBI_record(record_id)
+    mahar_fasta.write(record[0] + "\n" + str(record[1].seq) + "\n")
 
 
-retrieve_NCBI_record("MF421603")
