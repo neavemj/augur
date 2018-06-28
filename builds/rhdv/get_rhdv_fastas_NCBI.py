d664512c13cd2ba483d9195aa8c9b08e9ef8cd21
neavemj <matthewjneave1@gmail.com>
2018-06-28 11:28:04 +1000

    added function to get NCBI genbank record

--- builds/rhdv/get_rhdv_fastas_NCBI.py
+++ builds/rhdv/get_rhdv_fastas_NCBI.py
@@ -2,13 +2,24 @@ from Bio import Entrez
 from Bio import SeqIO
 
 # download RHDV genomes from NCBI with required header metadata
+# Matthew J. Neave 28.06.2018
 
-record_id = "MF421563"
+# get NCBI sequence IDs from Mahar et al. 2017. J. Virol
+# these ids range from MF421563 - MF421701
+
+mahar_ids = ["MF" + str(rec) for rec in range(421563, 421702)]
+
+# use biopython go online and retrieve these records
+# need to enter an email so users don't overuse their servers
 
 Entrez.email = "matthewjneave1@gmail.com"
 
-new_handle = Entrez.efetch(db="nucleotide", id=record_id, rettype="gb", retmode="genbank")
+def retrieve_NCBI_record(NCBI_ID):
+    new_handle = Entrez.efetch(db="nucleotide", id=NCBI_ID, rettype="gb", retmode="genbank")
+    seq_record = SeqIO.read(new_handle, "genbank")
+    collection_date = seq_record.features[0].qualifiers["collection_date"]
+    print(seq_record.features[0].qualifiers)
 
-seq_record = SeqIO.read(new_handle, "genbank")
 
-print(seq_record.features[0].qualifiers["collection_date"])
+
+retrieve_NCBI_record("MF421603")
